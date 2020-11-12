4701a1c90e5bc61c9d54867137057dd2976cc404
Far.k <farzana.mkarim@gmail.com>
2020-11-12 11:38:10 -0600

    message

--- src/App.js
+++ src/App.js
@@ -4,17 +4,57 @@ import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
 class App extends React.Component {
+  state = {
+    allPoems: [],
+    read: false
+  }
+
+  componentDidMount () {
+    fetch("http://localhost:6001/poems")
+    .then(r => r.json())
+    .then(allPoems => {
+      this.setState({allPoems})
+    })
+  }
+
+  addNewPoem = (poem) => {
+    let postOption ={
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(poem),
+    }
+    fetch("http://localhost:6001/poems", postOption)
+    .then(r => r.json())
+    .then(newPoem => {
+      this.setState({
+        allPoems: [...this.state.allPoems, newPoem]
+          })
+    })
+  }
+  readPoems = () => {
+    this.setState(
+      prevState => {
+        return {read: !prevState.read}
+      }
+    )
+  }
   render() {
     return (
       <div className="app">
         <div className="sidebar">
           <button>Show/hide new poem form</button>
-          {false && <NewPoemForm />}
+          {false && <NewPoemForm addNewPoem={this.addNewPoem}/>}
         </div>
-        <PoemsContainer />
+        <PoemsContainer allPoems={this.state.allPoems} />
       </div>
     );
   }
 }
 
 export default App;
+
+
+
