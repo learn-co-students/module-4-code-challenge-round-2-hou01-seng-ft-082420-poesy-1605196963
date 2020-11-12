83ce289be966d7e4ab55f8436a64cdb5fa6428de
bebomwonyi <bebomwonyi@gmail.com>
2020-11-12 11:34:23 -0600

    done

--- src/App.js
+++ src/App.js
@@ -4,14 +4,72 @@ import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
 class App extends React.Component {
+  state={
+    click: false,
+    poem: []
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:6001/poems')
+    .then(res=>res.json())
+    .then(data=>{
+      this.setState({
+        poem: data
+      })
+    })
+  }
+
+  addPoem=(poem)=>{
+    console.log(poem.target[0].value)
+    let title = poem.target[0].value
+    let auth = poem.target[1].value
+    let cont = poem.target[2].value
+
+
+    fetch('http://localhost:6001/poems',{
+      method: 'Post',
+      headers:{
+        "Content-type":"application/json",
+        "Accept":"application/json"
+      },
+      body: JSON.stringify({
+        'title': title,
+        "content":cont,
+        "author": auth
+      })
+    })
+    .then(res => res.json())
+    .then(newpoem =>{
+      return this.setState({
+        poem: [...this.state.poem, newpoem]
+      })
+    })
+  }
+
+  delete=(poem)=>{
+    console.log("delete")
+    let pd = this.state.poem.filter(p => p.id !== poem.id)
+
+    fetch(`http://localhost:6001/poems/${poem.id}`,{
+      method: 'DELETE'
+    })
+    .then(this.setState({poem: pd}))
+  }
+
+  handleClick=()=>{
+    let clicked = !this.state.click
+    this.setState({
+      click: clicked
+    })
+  }
   render() {
     return (
       <div className="app">
         <div className="sidebar">
-          <button>Show/hide new poem form</button>
-          {false && <NewPoemForm />}
+          <button onClick={this.handleClick}>Show/hide new poem form</button>
+          {this.state.click && <NewPoemForm addPoem={this.addPoem}/>}
         </div>
-        <PoemsContainer />
+        <PoemsContainer poem={this.state.poem} delete={this.delete}/>
       </div>
     );
   }
--- src/Poem.js
+++ src/Poem.js
@@ -1,15 +1,27 @@
 import React from "react";
 
 class Poem extends React.Component {
+  state={
+    read: true
+  }
+
+  handleRead=()=>{
+    let boo = !this.state.read
+    this.setState({
+      read: boo
+    })
+  }
+
   render() {
     return (
       <div>
-        <h3>Title</h3>
-        <p>Content</p>
+        <h3>{this.props.poem.title}</h3>
+        <p>{this.props.poem.content}</p>
         <p>
-          <strong>- By Author</strong>
+          <strong>- By {this.props.poem.author}</strong>
         </p>
-        <button>Mark as read</button>
+        <button onClick={this.handleRead}>{this.state.read ? "Mark as read" : "Mark as unread" }</button>
+        <button onClick={(e)=> this.props.delete(this.props.poem)}>Delete</button>
       </div>
     );
   }
