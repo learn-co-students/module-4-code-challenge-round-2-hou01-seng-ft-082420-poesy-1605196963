cf1bb0054e86f51b897668da0b5de0158b61fb90
SaintHugo <hugor37@gmail.com>
2020-11-12 11:37:35 -0600

    submission for deadline

--- src/NewPoemForm.js
+++ src/NewPoemForm.js
@@ -1,9 +1,37 @@
 import React from "react";
 
 class NewPoemForm extends React.Component {
+
+  state = {
+    title: "",
+    content: "",
+    author: ""
+  }
+  
+  handleSubmit = (e) => {
+    e.preventDefault()
+    // console.log(e.target[0].value, e.target[1].value, e.target[2].value,)
+    this.setState({
+      title: e.target[0].value,
+      content: e.target[1].value,
+      author: e.target[2].value
+    })
+    
+    fetch("http://localhost:6001/poems", {
+      method: "POST",
+      headers: {
+        "content-type":"application/json",
+        "accept":"application/json"
+      },
+      body: JSON.stringify()
+    })
+    .then(resp => resp.json())
+    .then(post => console.log(post))
+  }
+
   render() {
     return (
-      <form className="new-poem-form">
+      <form className="new-poem-form" onSubmit={this.handleSubmit} >
         <input placeholder="Title" />
         <input placeholder="Author" />
         <textarea placeholder="Write your masterpiece here..." rows={10} />
