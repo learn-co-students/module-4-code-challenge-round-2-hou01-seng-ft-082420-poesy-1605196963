73168a6ab1beb966268c4715e37f6fa7ba3dfcac
wellyyy <wesleydfaircloth@gmail.com>
2020-11-12 11:39:58 -0600

    submit yerrr

--- src/App.js
+++ src/App.js
@@ -3,15 +3,78 @@ import "./App.css";
 import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
+const url = 'http://localhost:6001/poems'
+
+
 class App extends React.Component {
+  state = {
+    allPoems: [],
+    form: true
+  }
+  
+  componentDidMount(){
+    fetch(url)
+    .then(res => res.json())
+    .then(data => this.setState({allPoems: data}))
+  }
+
+
+  onSubmit = e => { 
+    e.preventDefault()
+    fetch(url,{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        title: e.target.title.value,
+        author: e.target.author.value,
+        content: e.target.content.value
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        allPoems: [...this.state.allPoems, data]
+      })
+    })
+
+  }
+
+  form = e => {
+    if(this.state.form === true){
+      this.setState({
+        form: false
+      })
+
+    }
+    if(this.state.form === false){
+      this.setState({
+        form: true
+      })
+    }
+
+  }
+
+  onClick = e => {
+    console.log(e.target.innerText, "omgomgomgomgomgomgomgomgomgomgpogpogpogpogpogpog")
+    if(e.target.innerText === "Mark as read"){
+      e.target.innerText = "Mark as unread"
+    }
+    else if(e.target.innerText === "Mark as unread"){
+      e.target.innerText = "Mark as read"
+    }
+  }
+
   render() {
     return (
       <div className="app">
         <div className="sidebar">
-          <button>Show/hide new poem form</button>
-          {false && <NewPoemForm />}
-        </div>
-        <PoemsContainer />
+          <button onClick={this.form}>Show/hide new poem form</button>
+          {this.state.form && <NewPoemForm addPoem={this.addPoem} handleSubmit={this.onSubmit}/>}
+        </div>  
+        <PoemsContainer allPoems={this.state.allPoems} onClick={this.onClick}/>
       </div>
     );
   }
--- src/NewPoemForm.js
+++ src/NewPoemForm.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 class NewPoemForm extends React.Component {
+  
   render() {
+    console.log(this.props)
     return (
-      <form className="new-poem-form">
-        <input placeholder="Title" />
-        <input placeholder="Author" />
-        <textarea placeholder="Write your masterpiece here..." rows={10} />
+      <form className="new-poem-form" onSubmit={this.props.handleSubmit}>
+        <input name= "title"placeholder="Title" />
+        <input name= "author"placeholder="Author" />
+        <textarea name= "content" placeholder="Write your masterpiece here..." rows={10} />
         <input type="submit" value="Share your masterpiece" />
       </form>
     );
