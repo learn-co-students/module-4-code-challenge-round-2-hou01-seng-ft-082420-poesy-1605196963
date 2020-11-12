1260ebbc8f51de499ce0ddcbbd176427a07b8975
github username <github email address>
2020-11-12 11:38:44 -0600

    done

--- src/App.js
+++ src/App.js
@@ -4,14 +4,67 @@ import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
 class App extends React.Component {
+
+  state = {
+    poems: [],
+    showForm: false
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:6001/poems')
+      .then(res => res.json())
+      .then(poemData => {this.setState({poems: poemData})})
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+    console.log(event.target.title.value)
+    fetch('http://localhost:6001/poems', {
+      method:'POST',
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        title: event.target.title.value,
+        content: event.target.content.value,
+        author: event.target.author.value
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      let poems = [...this.state.poems, data]
+      this.setState({poems: poems})
+    })
+  }
+
+  handleRead = event => {
+    console.log(event.target.innerText)
+    const text = event.target.innerText
+
+    if(text === "Mark as read"){
+      event.target.innerText = "Mark as unread"
+    }else{
+      event.target.innerText = "Mark as read"
+    }
+  }
+
+  handleForrm = event => {
+    if(this.state.showForm === false){
+      this.setState({showForm: true})
+    }else{
+      this.setState({showForm: false})
+    }
+  }
+
   render() {
     return (
       <div className="app">
         <div className="sidebar">
-          <button>Show/hide new poem form</button>
-          {false && <NewPoemForm />}
+          <button onClick={this.handleForrm}>Show/hide new poem form</button>
+          {this.state.showForm && <NewPoemForm onSubmit={this.onSubmit}/>}
         </div>
-        <PoemsContainer />
+        <PoemsContainer state={this.state} handleRead={this.handleRead}/>
       </div>
     );
   }
--- src/NewPoemForm.js
+++ src/NewPoemForm.js
@@ -3,10 +3,10 @@ import React from "react";
 class NewPoemForm extends React.Component {
   render() {
     return (
-      <form className="new-poem-form">
-        <input placeholder="Title" />
-        <input placeholder="Author" />
-        <textarea placeholder="Write your masterpiece here..." rows={10} />
+      <form className="new-poem-form" onSubmit={this.props.onSubmit}>
+        <input placeholder="Title" name="title"/>
+        <input placeholder="Author" name="author"/>
+        <textarea placeholder="Write your masterpiece here..." rows={10} name="content"/>
         <input type="submit" value="Share your masterpiece" />
       </form>
     );
